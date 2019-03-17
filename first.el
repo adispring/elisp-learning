c62117ca5935ea5b4a94ff22d1004ba18fbca82a
adispring <sunnyadi@163.com>
2019-03-17 12:56:53 +0800

    save first.el

--- first.el
+++ first.el
@@ -1,2 +1,33 @@
 (string-match
  "js\\(x\\)?$" "The quick brown fox jumped quickly.jsx")
+
+(set (intern "abc" foo) "I'm abc")
+
+(intern-soft "abc" foo)
+
+(symbolp '+1)
+nil
+
+(symbolp '\+1)
+t
+
+(setq foo (make-vector 10 0))
+[0 0 0 0 0 0 0 0 0 0]
+(intern-soft "abc" foo)
+nil
+(intern "abc" foo)
+abc
+(intern-soft "abc" foo)
+abc
+
+(unintern "abc" foo)
+t
+(intern-soft "abc" foo)
+nil
+
+
+
+
+
+
+
